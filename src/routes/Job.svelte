88cbd8424dfa87f1dbfcda7e5e2083bfb601951c
Nathan Lindsay <993035+nathanlindsay@users.noreply.github.com>
2023-09-26 16:52:45 +0100

    Homepage tidy

---
+++ src/routes/Job.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let title: string;
+	export let where: string | undefined = undefined;
+	export let when: string;
+</script>
+
+<li
+	class="block space-y-2 overflow-hidden rounded-lg bg-white p-6 shadow-lg ring-1 ring-gray-950/10 transition duration-300 print:shadow-none"
+>
+	<div>
+		<h5 class="font-medium">{title}</h5>
+
+		{#if where}
+			<p>{where}</p>
+		{/if}
+
+		<p class="text-sm">{when}</p>
+	</div>
+
+	<p>
+		<slot />
+	</p>
+</li>
